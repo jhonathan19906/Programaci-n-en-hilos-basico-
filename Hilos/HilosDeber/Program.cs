c85ec170aa2ca8febb6463dd629d7df076343117
jhonathan19906 <47836888+jhonathan19906@users.noreply.github.com>
2020-08-13 18:57:38 -0500

    Manejo de hilos básico en c#
    Este pequeño programa muestra el manejo de hilos básico en c# 
    Creado por Jhonathan Chiliquinga 
    Curso Tercero  "A" de desarrollo de software

---
+++ Hilos/HilosDeber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace HilosDeber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(metodo);
+            Thread e = new Thread(metodo1);
+            Thread r = new Thread(metodo2);
+            e.Start();
+            r.Start();
+            t.Start();
+            Thread.Sleep(2000);
+            Console.Write("Hilos finalisados");
+            Console.Read();
+        }
+        static void metodo()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("hola ");
+            }
+        }
+        static void metodo1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("que mas");
+            }
+        }
+        static void metodo2()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("amigos");
+            }
+        }
+    }
+}
